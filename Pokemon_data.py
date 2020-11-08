bc2e0866aefa5dd37232b104ceb00a65e2597bef
chanhomin <chanhomin@ajou.ac.kr>
2020-11-08 22:14:48 +0900

    Git

--- Pokemon_data.py
+++ Pokemon_data.py
@@ -10,7 +10,14 @@ class Pokemon:
         self.speed=0
 class Tortoise(Pokemon):
     def __init__(self):
-        pass
+        self.level=5
+        self.exp_point=0
+        self.health=0
+        self.attack=0
+        self.block=0
+        self.contact=0
+        self.defense=0
+        self.speed=0
 
 class Pikachu(Pokemon):
     def __init__(self):
