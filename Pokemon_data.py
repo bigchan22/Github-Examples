16e04e02e6fb17c398c9485c339185dd7c4cdab7
chanhomin <chanhomin@ajou.ac.kr>
2020-11-08 23:43:12 +0900

    Pikachu의 data

--- Pokemon_data.py
+++ Pokemon_data.py
@@ -21,4 +21,11 @@ class Tortoise(Pokemon):
 
 class Pikachu(Pokemon):
     def __init__(self):
-        pass
+        self.level=5
+        self.exp_point=0
+        self.health=0
+        self.attack=0
+        self.block=0
+        self.contact=0
+        self.defense=0
+        self.speed=20
